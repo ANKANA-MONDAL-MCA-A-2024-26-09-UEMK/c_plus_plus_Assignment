4a633becac7d2ae5eb4df5a70b4bcef1e3ff3a0c
ANKANA-MONDAL-MCA-A-2024-26-09-UEMK <mondalankana54@gmail.com>
2025-06-11 12:45:56 +0530

    11/06/2025

---
+++ callByValue/BeforeModityAfterModity.cpp
@@ -0,0 +1,17 @@
+// CallByValue.cpp
+#include <iostream>
+using namespace std;
+
+// Function to demonstrate call by value
+void modify(int x) {
+    x = x + 10;
+    cout << "Inside modify() - Value of x: " << x << endl;
+}
+
+int main() {
+    int a = 5;
+    cout << "Before modify() - Value of a: " << a << endl;
+    modify(a);  // Call by value
+    cout << "After modify() - Value of a: " << a << endl;
+    return 0;
+}
